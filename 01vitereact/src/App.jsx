1bbe1f2866101b4f7f82b682a7b68b9a824cac81
Chayan <chayansamanta406@gmail.com>
2025-06-28 17:40:50 +0530

    learn basics

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Chai from "./chai.jsx"
+import Hello from "./hello.jsx"
+
+function App() {
+
+  return (
+    <>
+      <Chai />
+      <h1>chai aur code</h1>
+      <p>hello world</p>
+      <Hello />
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/hello.jsx
@@ -0,0 +1,10 @@
+function Hello() {
+    return (
+        <>
+        <label>Enter Name : </label>
+        <input type="text" />
+        </>
+    )
+}
+
+export default Hello
