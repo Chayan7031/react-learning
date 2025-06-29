6d003f8c2d6c87af727939ba38007bb53357b970
Chayan <chayansamanta406@gmail.com>
2025-06-29 19:33:24 +0530

    modify

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,12 +3,14 @@ import Chai from "./chai.jsx"
 import Hello from "./hello.jsx"
 
 function App() {
+  const name = 'chayan Samanta'
+  //{name} : Evaluation expression --> if we want to edit we have to do it outside return not inside {name}
 
   return (
     <>
       <Chai />
       <h1>chai aur code</h1>
-      <p>hello world i am chayan</p>
+        <p>hello world i am {name}</p> 
       <Hello />
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,42 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp (){
+  return(
+    <div>
+      <h1>Chayan Samanta !</h1>
+    </div>
+  )
+}
+
+/*const reactElement = {
+    type: 'a',
+    props: {
+        href : 'https://google.com',
+        target: '_blank'
+    },
+    Children: 'Click me to visit google'
+}*/
+
+const anotherUser = 'chayan samanta'
+
+const reactElement = React.createElement(
+  'a',
+  {href : 'https://google.com', target: '_blank'},
+  'Click me to visit google ',
+  anotherUser
+)
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>visit google</a>
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  /*<StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>,*/
+  // anotherElement
+  reactElement
 )
